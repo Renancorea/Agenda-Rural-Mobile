d9c140db8948a89bdfe45a7a3ee49dd6ee010f03
Ana Borges <anaclaralimaborges80@gmail.com>
2026-06-03 07:53:31 -0300

    instalação do expo-sqlite, criação do database e layout.

---
+++ src/app/database/database.ts
@@ -0,0 +1,12 @@
+import { type SQLiteDatabase} from 'expo-sqlite';
+
+export async function initializeDatabase( database: SQLiteDatabase) { //Função para criar a tabela de agricultores, caso ela ainda não existisse .
+	await database.execAsync(` 
+        CREATE TABLE IF NOT EXISTS agricultores( 
+            id INTEGER PRIMARY KEY AUTOINCREMENT, 
+            nome TEXT NOT NULL, 
+            email TEXT NOT NULL UNIQUE, 
+            senha TEXT NOT NULL  
+        )
+    `)
+}
