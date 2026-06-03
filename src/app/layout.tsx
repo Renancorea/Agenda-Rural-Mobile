f361af32e8c8858c65d04bb40fefd09da4e1f28b
Renan <renancorreia730@gmail.com>
2026-06-03 08:43:14 -0300

    Adicionando o Banco de dados SQlite diretamente

---
+++ src/app/layout.tsx
@@ -0,0 +1,12 @@
+import { Slot} from 'expo-router'; // usado para redecionar para as telas "filhas".
+import { SQLiteDatabase, SQLiteProvider } from 'expo-sqlite'; // importa os componentes necessarios.
+import { initializeDatabase } from './database/database'; // importa a função de inicialização do banco de dados.
+
+export default function Layout() { // componente de layout que envolve as telas do aplicativo e da acesso ao banco de dados.
+    return(
+        <SQLiteProvider databaseName="AgendaRural.db" onInit={initializeDatabase}> // fornece o banco de dados para as telas filhas, garantindo que ele seja inicializado antes do uso.
+              <Slot/>
+        </SQLiteProvider>
+      
+    );
+}
